d5f5c0fa1d95597abe3214807f7697a6dc3cb7ee
AsbjornOlling <asbjornolling@gmail.com>
2017-12-08 08:30:19 +0100

    fixed parsing bug

--- 07/tree.py
+++ 07/tree.py
@@ -3,10 +3,14 @@
 
 class Program:
     def __init__(self, line):
-        self.name = line[:5]
+        # remove newline char
+        line = line[:len(line)-1]
+        # then parse line
+        self.name = line[:4]
         self.weight = line[6:8]
         children_string = line[13:]
-        self.children_list = [] # so it doesn't return error when pulling list
+        # then make list of children
+        self.children_list = [] # avoid error when pulling list
         if len(children_string) > 0:
             self.children_list = children_string.split(", ")
 
@@ -24,18 +28,7 @@ for program in list_of_programs:
     for child in program.children_list:
         all_children.append(child)
 
-print("children:")
-for child in all_children:
-    print(child)
-print("children done")
-
 # then look for programs who's nobodys child:
 for program in list_of_programs:
-    # only check programs that have children
-    if len(program.children_list) > 0:
+    if not (program.name in all_children):
         print(program.name)
-
-        # this statement doesn't work
-        if not program.name in all_children:
-            #print(program.name)
-            print(program.name)
