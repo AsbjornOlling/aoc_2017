31fe1d3db9d0dc9f3ccb25bba9e18cb877c83759
AsbjornOlling <asbjornolling@gmail.com>
2017-12-11 03:17:02 +0100

    finished but broken

--- 08/registers.py
+++ 08/registers.py
@@ -2,18 +2,14 @@
 
 # initialise vars to use
 register_db = {}
+line_no = 0
 
-def change_register(action, amount, register):
-    if not (register in register_db):
-        register_db[register] = 0
-    if action == "inc":
-        register_db[register] += amount
-    elif action == "dec":
-        register_db[register] -= amount
 
 # read input file
-for line in open("test.txt"):
+for line in open("input.txt"):
     line_array = line.strip("\n").split(" ")
+
+    line_no += 1
 
     # parse one line
     register = line_array[0]
@@ -23,17 +19,35 @@ for line in open("test.txt"):
     condition_operator = line_array[5]
     condition_amount = line_array[6]
 
-    if condition_operator == ">" and condition_register > condition_amount:
-        change_register(action, amount, register)
-    elif condition_operator == ">=" and condition_register >= condition_amount:
-        change_register(action, amount, register)
-    elif condition_operator == "<" and condition_register < condition_amount:
-        change_register(action, amount, register)
-    elif condition_operator == "<=" and condition_register <= condition_amount:
-        change_register(action, amount, register)
-    elif condition_operator == "!=" and condition_register != condition_amount:
-        change_register(action, amount, register)
-    elif condition_operator == "==" and condition_register == condition_amount:
-        change_register(action, amount, register)
+    # check if condition met
+    if (
+           (condition_operator == ">" 
+            and condition_register > condition_amount)
+        or (condition_operator == "<" 
+            and condition_register < condition_amount)
+        or (condition_operator == ">=" 
+            and condition_register >= condition_amount)
+        or (condition_operator == "<=" 
+            and condition_register <= condition_amount)
+        or (condition_operator == "!=" 
+            and condition_register != condition_amount)
+        or (condition_operator == "==" 
+            and condition_register == condition_amount)
+    ):
+        print(line+" on "+line_no+" met!")
+	# increment or decrement if met
+	if not (register in register_db):
+	    register_db[register] = 0
+	if action == "inc":
+	    register_db[register] += amount
+            print("Incrementing "+register)
+	elif action == "dec":
+	    register_db[register] -= amount
+            print("Decemenenting "+register)
 
-print(max(register_db.iterkeys(), key=(lambda key: register_db[register])))
+# find highest value (answer to challenge 1)
+highest_value = 0
+for key, value in register_db.items():
+    if value > highest_value:
+        highest_value = value
+print(str(highest_value))
