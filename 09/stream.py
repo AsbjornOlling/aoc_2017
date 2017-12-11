9d366209ce7bdfb58fb37774403742a4d6bc5900
AsbjornOlling <asbjornolling@gmail.com>
2017-12-12 00:40:22 +0100

    day 8 complete

--- 09/stream.py
+++ 09/stream.py
@@ -1,20 +1,23 @@
-# Dec 09, challenge 1
-# GOLD STAR GOT
+# Dec 09, challenge 1+2
+# GOLD STARS GOT
 
 # load input file
 # main input file is only one line
 for line in open("input.txt"):
+    line = line.strip("\n")
 
     # start vars
     group_level = 0
     points = 0
+    garbage_count = 0
 
     # first go through and handle escape chars
+    # only O(2*n), but ok
     i = 0
     while i < len(line):
         # remove chars after !
         if line[i] == "!":
-            line = line[:i] + " " + line[i+2:]
+            line = line[:i] + "``" + line[i+2:]
         i += 1
 
     # then do the rest
@@ -33,8 +36,12 @@ for line in open("input.txt"):
         elif line[i] == "<":
             while line[i] != ">":
                 i += 1
+                # count garbage (answer to challenge 2)
+                if not (line[i] in ["`",">"]):
+                    garbage_count += 1
 
         # move on to next line[i]
         i += 1
 
-    print(str(points))
+    #print(str(points))
+    print(str(garbage_count))
