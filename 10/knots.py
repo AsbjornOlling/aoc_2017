7b6e5f39b23cff8f9fa80b4acc8205006bf21ad3
AsbjornOlling <asbjornolling@gmail.com>
2017-12-13 14:21:38 +0100

    new input processing

--- 10/knots.py
+++ 10/knots.py
@@ -1,5 +1,11 @@
-# December 10, challenge 1
-# GOLD STAR GOT
+# December 10, challenge 2
+# GOLD STAR GOT on challenge 1
+
+## TODO
+
+# read ascii in input file
+# do 64 runs total (preserving cur_pos and skip_size)
+# then bitwise xor blocks of 16 ( so you have 16 nums ) 
 
 # init vars
 cur_pos = 0
@@ -8,24 +14,31 @@ list_length = 256
 num_list = []
 for i in range(0, list_length):
     num_list.append(i)
+suffix = [17, 31, 73, 47, 23]
 
 
 # read input
-input_file = open("input.txt")
+input_file = open("test.txt")
 
 # really, it's only one line
 # so this is the main block of code
 for line in input_file:
 
-    # read lengths into list
-    input_lengths = line.strip("\n").split(",")
+    # convert each char to an ascii code
+    line = line.strip("\n")
+    input_lengths = []
+    for char in line:
+        input_lengths.append(ord(char))
+    for num in suffix:
+        input_lengths.append(num)
+    print(input_lengths)
 
     # main loop
     # extract and reverse substring
     for substring_length in input_lengths:
         # extract substring
         substring = []
-        for i in range(0, int(substring_length)):
+        for i in range(0, substring_length):
             substring.append(num_list[(cur_pos + i) % list_length])
 
         # reverse substring
@@ -33,7 +46,7 @@ for line in input_file:
         #print(substring)
 
         # now place substring back on num_list
-        for i in range(0, int(substring_length)):
+        for i in range(0, substring_length):
             num_list[(cur_pos + i) % list_length] = substring[i]
 
         #print(num_list)
