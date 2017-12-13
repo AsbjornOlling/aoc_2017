35d14f5e3a2bc2fb27f82e6b69bb6a2686b2b78c
AsbjornOlling <asbjornolling@gmail.com>
2017-12-13 13:12:07 +0100

    works on test data

--- 10/knots.py
+++ 10/knots.py
@@ -3,7 +3,7 @@
 # init vars
 cur_pos = 0
 skip_size = 0
-list_length = 5
+list_length = 256
 num_list = []
 for i in range(0, list_length):
     num_list.append(i)
@@ -29,10 +29,15 @@ for line in input_file:
 
         # reverse substring
         substring = substring[::-1]
+        print(substring)
 
         # now place substring back on num_list
-        for i in range(cur_pos, cur_pos + int(substring_length)):
-            num_list[i % list_length] = 0
+        for i in range(0, int(substring_length)):
+            num_list[(cur_pos + i) % list_length] = substring[i]
+
+        print(num_list)
 
         cur_pos += int(substring_length) + skip_size
         skip_size += 1
+
+    print(num_list[0] * num_list[1])
